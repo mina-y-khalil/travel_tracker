da4f79809f55248babdc403816ff978db2b44e3d
mina-y-Khalil <mina.y.khalil90@gmail.com>
2025-10-08 22:54:20 -0700

    Add functionality for inserting a new country

--- index.js
+++ index.js
@@ -21,15 +21,59 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+// GET home page
 app.get("/", async (req, res) => {
-  const results = await db.query("SELECT * FROM visited_countries");
-  let countries = []
-  results.rows.forEach(row => countries.push(row.country_code));
-  console.log(results.rows);
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
 });
 
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      [input]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
